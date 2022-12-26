58d7b1193ab2d6f50b8c7ea907c842be0e442f24
afjal khan <afjalsolanki07@gmail.com>
2022-12-26 12:44:00 +0530

    add git.js file

---
+++ src/comman/git.js
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+// git status
+// git add .
+// git status
+// git commit -m 'any message'
+// git push
+
+const git = () => {
+  return (
+    <View>
+      <Text>git</Text>
+    </View>
+  );
+};
+
+export default git;
+
+const styles = StyleSheet.create({});
